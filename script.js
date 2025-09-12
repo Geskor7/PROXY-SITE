9f1827cd7a84b40759c12ca7cc9fd68bacae03ab
Geskor7 <glubglib481@gmail.com>
2025-09-12 23:09:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // --- START: YOUR CUSTOM QUOTES ---
 const quotes = [
-  { quote: 'There’s no place like 127.0.0.1', author: 'Austin Freeman' },
-  { quote: 'doing "𝗻𝗺𝗮𝗽 -𝗧𝟰 127.0.0.1" doesnt make you scary', author: 'r' },
-  { quote: "i want sprite :'(", author: 'RAX' }
+  { quote: `There’s no place like 127.0.0.1`, author: `Austin Freeman` },
+  { quote: `doing "𝗻𝗺𝗮𝗽 -𝗧𝟰 127.0.0.1" doesnt make you scary`, author: `r` },
+  { quote: `i want sprite :'(`, author: `RAX` }
 ];
+
 
 // --- END: YOUR CUSTOM QUOTES ---
 
